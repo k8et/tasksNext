782c48289e9cea01c9eaaeb264afff0ff1a387e6
TeamLead <mxmgnchrv@gmail.com>
2024-11-17 20:39:13 +0200

    feat: fix adaptive

--- src/app/csr/page.tsx
+++ src/app/csr/page.tsx
@@ -36,7 +36,7 @@ const CSRPage: FC = () => {
     }
 
     return (
-        <div className="max-w-4xl mx-auto p-6">
+        <div className="max-w-4xl mx-auto p-6 overflow-auto max-md:w-full">
             <h1 className="text-3xl font-bold text-center mb-6">CSR</h1>
             <UserList users={users || []} />
         </div>
--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
     const users = await fetchUsers();
 
     return (
-        <div className="max-w-4xl mx-auto p-6">
+        <div className="max-w-4xl mx-auto p-6 overflow-auto max-md:w-full   justify-center flex-col">
             <h1 className="text-3xl font-bold text-center mb-6">SSR</h1>
             <UserList users={users} />
         </div>
